f978ddb617867bacacc48ad999677faa63e020a9
Valentyn Kasyan <vkasyan@gmx.de>
2023-10-19 20:37:37 +0200

    API Limit Utils & Prisma DB

--- app/api/code/route.ts
+++ app/api/code/route.ts
@@ -3,6 +3,8 @@ import { NextResponse } from "next/server";
 import OpenAI from "openai";
 import { ChatCompletionMessage } from "openai/resources/chat/index.mjs";
 
+import { increaseApiLimit, checkApiLimit } from "@/lib/api-limit";
+
 const openai = new OpenAI({
   apiKey: process.env.NEXT_PUBLIC_OPEN_API_KEY,
 });
@@ -31,10 +33,18 @@ export async function POST(req: Request) {
       return new NextResponse("Messages are required", { status: 400 });
     }
 
+    const freeTrial = await checkApiLimit();
+
+    if (!freeTrial) {
+      return new NextResponse("Free trial has expired.", { status: 403 });
+    }
+
     const response = await openai.chat.completions.create({
       model: "gpt-3.5-turbo",
       messages: [instructionMessage, ...messages],
     });
+
+    await increaseApiLimit();
 
     return NextResponse.json(response.choices[0].message);
   } catch (error) {
